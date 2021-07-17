133ffa43b34477729e631fce66394b94aa9aebf9
dhk06 <tommydhk@naver.com>
2021-07-18 00:42:41 +0900

    proj ver1

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const title = document.querySelector('.title');
 const introText = document.querySelector('.introText');
 const text1 = document.querySelector('.text1');
 const text2 = document.querySelector('.text2');
+const de_say = document.querySelector('#de_say');
+const downArrow = document.querySelector('.downArrow');
 
 let sideMenuCheck = false;
 
@@ -16,16 +18,20 @@ window.addEventListener("scroll", () => {
     if(sideMenuCheck == false){
         if (window.scrollY > headerHeight) {
             header.setAttribute("style", "background: white; color: black;");
+            downArrow.setAttribute("style", "filter: opacity(0%)");
+            de_say.setAttribute("style", "transform: translateY(30px)");
             title.style.color = 'black';
         } else {
             header.setAttribute("style", "background: transparent; color: white;");
+            downArrow.setAttribute("style", "filter: opacity(100%)");
+            de_say.setAttribute("style", "transform: translateY(0px)");
             title.style.color = 'white';
         }
     }
 });
 
 sideMenuBtn.onclick = () =>{
-    if(sideMenuCheck == false){
+    if(sideMenuCheck == false){ 
         sideMenuCheck = true;
         sideMenu.style.transform = 'translate(0%)';
         sideMenu.style.filter = 'opacity(100%)';
@@ -41,4 +47,3 @@ sideMenuBtn.onclick = () =>{
         }
     }
 }
-
